6ea87215d3aba589d3845871ceb1a1fb4743b803
jcclark141152 <jcclark141152@gmail.com>
2021-02-19 13:44:02 -1000

    Update loading and preprocessing

--- load_and_preprocess.py
+++ load_and_preprocess.py
@@ -8,10 +8,12 @@ def concatenate():
 
     for f in files:
         data = pd.read_csv(os.path.join(settings.DATA_DIR, f)).set_index('Unnamed: 0')
-        data.index.name = None
-
+        data['Role'] = os.path.basename(f).split('.')[0].replace('_',' ').title()
         full_df.append(data)
-    full_df = pd.concat(full_df, axis = 1)
+
+    full_df = pd.concat(full_df)
+    full_df.index.name = None
+    full_df.reset_index(drop = True)
     full_df.to_csv(os.path.join(settings.PROCESSED_DIR, 'data.csv'))
 
 if __name__ == "__main__":
